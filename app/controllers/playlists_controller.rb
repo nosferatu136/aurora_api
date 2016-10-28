6bd925575ceee35d75cba53039b55a17e5502478
nosferatu136 <nosferatu136@hotmail.com>
2016-10-27 18:43:50 -0700

    controllers: DRY albums and playlists controllers.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -18,43 +18,10 @@ class PlaylistsController < CommonMusicController
     [:name, :user_id]
   end
 
-  class PlaylistSongProcessor
-    attr_reader :playlist_id, :song_ids, :action
-
+  class PlaylistSongProcessor < SongListProcessor
     def initialize(params, action)
-      @playlist_id = params[:playlist_id]
-      @song_ids = params[:song_ids]
-      @action = action
-    end
-
-    def process
-      if playlist
-        if action == :add
-          playlist_song_ids = playlist.songs.pluck(:id)
-          songs_to_add_ids = song_ids.map(&:to_i) - playlist_song_ids
-          Song.where(id: songs_to_add_ids).each do |song|
-            playlist.playlist_songs.create(song_id: song.id)
-          end
-        else
-          PlaylistSong.where(playlist_id: playlist_id, song_id: song_ids).delete_all
-        end
-      end
-      self
-    end
-
-    def playlist
-      return @playlist if defined?(@playlist)
-      @playlist = Playlist.find_by_id(playlist_id)
-    end
-
-    def status
-      return :not_found unless playlist
-      errors.any? ? :unprocessable_entity : :ok
-    end
-
-    def errors
-      return [] unless playlist
-      playlist.reload.errors.full_messages
+      @type = :playlist
+      super
     end
   end
 end
